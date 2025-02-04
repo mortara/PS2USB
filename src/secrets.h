ec1257ebf47770053bdbd268b3e51feae48d5876
Patrick Mortara <mortara@gmail.com>
2025-02-04 13:53:49 +0100

    ...

--- src/secrets.h
+++ src/secrets.h
@@ -5,8 +5,4 @@
 #define WIFISSID "WELAHN2G"
 #define WIFIPASS "dukommsthiernichtrein"
 
-#define MQTTBROKER "192.168.10.101"
-#define MQTTUSER "homeassistant"
-#define MQTTPASSWORD "hapake79"
-
 #endif
