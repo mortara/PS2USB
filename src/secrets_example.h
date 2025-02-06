f999109b395f77a20924e10d7c7244e8c9b96be5
Patrick Mortara <mortara@gmail.com>
2025-02-06 09:49:09 +0100

    Added secrets.h example

---
+++ src/secrets_example.h
@@ -0,0 +1,10 @@
+/* Rename this file to secrets.h and put your WiFi credentials!*/
+
+
+#ifndef SECRETS_H
+#define SECRETS_H
+
+#define WIFISSID "MYWIFISSID"
+#define WIFIPASS "wifissidpassword"
+
+#endif
